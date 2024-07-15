5f451c44c733a483fff4426f84380438aa32a360
Hanimir-san <fou_lou2003@hotmail.com>
2024-07-15 07:06:39 +0200

    Added default umask settings. Added system wide read control file.

--- bash/init_system.sh
+++ bash/init_system.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Check that script is being run with user admin privileges
-if [ $USER != "root" ];
+if [ $EUID != 0 ];
 then
     echo "This script must be run with superuser privileges!"
     exit 1 
@@ -28,30 +28,37 @@ fi
 # Get appropriate read command file. Add other shell types as needed.
 if [ $SHELL == "/bin/zsh" ] ;
 then
-    RC_FILE=".zshrc"
+    RC_FILE="zshrc"
 elif [ $SHELL == "/bin/tcsh" ] ;
 then
-    RC_FILE=".tcshrc"
+    RC_FILE="tcshrc"
 elif [ $SHELL == "/bin/csh" ] ;
 then
-    RC_FILE=".cshrc"
+    RC_FILE="cshrc"
 else
-    RC_FILE=".bashrc"
+    RC_FILE="bashrc"
 fi
 
-RC_TEMPLATE="/etc/skel/$RC_FILE"
-RC_USER_CURRENT="$USER_CURRENT_HOME/$RC_FILE"
+RC_SYSTEM="/etc/$RC_FILE"
+RC_TEMPLATE="/etc/skel/.$RC_FILE"
+RC_USER_CURRENT="$USER_CURRENT_HOME/.$RC_FILE"
 
 # Create a dummy file for testing the script. This section will be removed later
+RC_SYSTEM=$RC_SYSTEM.test
 RC_TEMPLATE=$RC_TEMPLATE.test
 RC_USER_CURRENT=$RC_USER_CURRENT.test
 
+touch RC_SYSTEM
 touch RC_TEMPLATE
 touch RC_USER_CURRENT
 
 # Create backup of rc files 
+cp -pr $RC_SYSTEM $RC_SYSTEM.bak
 cp -pr $RC_TEMPLATE $RC_TEMPLATE.bak
 cp -pr $RC_USER_CURRENT $RC_USER_CURRENT.bak
+
+# Ensure system default umask is restrictive
+echo "umask 0022" > $RC_SYSTEM
 
 # Add common aliases to rc files
 for RC in $RC_TEMPLATE $RC_USER_CURRENT ;
